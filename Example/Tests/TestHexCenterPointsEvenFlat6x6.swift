d33adf0a8731f6160e169f288425e472e209c42a
zach lysobey <zach@lysobey.com>
2015-12-24 17:39:34 -0500

    extract 'assertTileCenterAtXY' test method

--- Example/Tests/TestHexCenterPointsEvenFlat6x6.swift
+++ Example/Tests/TestHexCenterPointsEvenFlat6x6.swift
@@ -40,29 +40,23 @@ class TestHexCenterPointsEvenFlat6x6: XCTestCase {
         let q : Int = 1
         var tile : Tile!
 
-        tile = HexKit.sharedInstance.tile(q:q, r:0)
-        XCTAssertEqualWithAccuracy (tile.center.x, 22.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 38.1051177665153, accuracy: accuracy, "Expected result not equal");
+        tile = HexKit.sharedInstance.tile(q:q, r:0);
+        assertTileCenterAtXY(tile, 22.0, 38.1051177665153);
 
-        tile = HexKit.sharedInstance.tile(q:q, r:1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 22.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 76.2102355330306, accuracy: accuracy, "Expected result not equal");
+        tile = HexKit.sharedInstance.tile(q:q, r:1);
+        assertTileCenterAtXY(tile, 22.0, 76.2102355330306);
 
-        tile = HexKit.sharedInstance.tile(q:q, r:2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 22.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 114.315353299546, accuracy: accuracy, "Expected result not equal");
+        tile = HexKit.sharedInstance.tile(q:q, r:2);
+        assertTileCenterAtXY(tile, 22.0, 114.315353299546);
 
-        tile = HexKit.sharedInstance.tile(q:q, r:3)
-        XCTAssertEqualWithAccuracy (tile.center.x, 22.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 152.420471066061, accuracy: accuracy, "Expected result not equal");
+        tile = HexKit.sharedInstance.tile(q:q, r:3);
+        assertTileCenterAtXY(tile, 22.0, 152.420471066061);
 
-        tile = HexKit.sharedInstance.tile(q:q, r:4)
-        XCTAssertEqualWithAccuracy (tile.center.x, 22.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 190.525588832576, accuracy: accuracy, "Expected result not equal");
+        tile = HexKit.sharedInstance.tile(q:q, r:4);
+        assertTileCenterAtXY(tile, 22.0, 190.525588832576);
 
-        tile = HexKit.sharedInstance.tile(q:q, r:5)
-        XCTAssertEqualWithAccuracy (tile.center.x, 22.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 228.630706599092, accuracy: accuracy, "Expected result not equal");
+        tile = HexKit.sharedInstance.tile(q:q, r:5);
+        assertTileCenterAtXY(tile, 22.0, 228.630706599092);
 
     }
 
@@ -72,28 +66,22 @@ class TestHexCenterPointsEvenFlat6x6: XCTestCase {
         var tile : Tile!
 
         tile = HexKit.sharedInstance.tile(q:q, r:-1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 55.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 19.0525588832576, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 55.0, 19.0525588832576);
 
         tile = HexKit.sharedInstance.tile(q:q, r:0)
-        XCTAssertEqualWithAccuracy (tile.center.x, 55.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 57.157676649773, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 55.0, 57.157676649773);
 
         tile = HexKit.sharedInstance.tile(q:q, r:1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 55.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 95.2627944162882, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 55.0, 95.2627944162882);
 
         tile = HexKit.sharedInstance.tile(q:q, r:2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 55.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 133.367912182804, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 55.0, 133.367912182804);
 
         tile = HexKit.sharedInstance.tile(q:q, r:3)
-        XCTAssertEqualWithAccuracy (tile.center.x, 55.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 171.473029949319, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 55.0, 171.473029949319);
 
         tile = HexKit.sharedInstance.tile(q:q, r:4)
-        XCTAssertEqualWithAccuracy (tile.center.x, 55.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 209.578147715834, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 55.0, 209.578147715834);
 
     }
 
@@ -103,28 +91,22 @@ class TestHexCenterPointsEvenFlat6x6: XCTestCase {
         var tile : Tile!
 
         tile = HexKit.sharedInstance.tile(q:q, r:-1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 88.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 38.1051177665153, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 88.0, 38.1051177665153);
 
         tile = HexKit.sharedInstance.tile(q:q, r:0)
-        XCTAssertEqualWithAccuracy (tile.center.x, 88.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 76.2102355330306, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 88.0, 76.2102355330306);
 
         tile = HexKit.sharedInstance.tile(q:q, r:1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 88.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 114.315353299546, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 88.0, 114.315353299546);
 
         tile = HexKit.sharedInstance.tile(q:q, r:2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 88.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 152.420471066061, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 88.0, 152.420471066061);
 
         tile = HexKit.sharedInstance.tile(q:q, r:3)
-        XCTAssertEqualWithAccuracy (tile.center.x, 88.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 190.525588832576, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 88.0, 190.525588832576);
 
         tile = HexKit.sharedInstance.tile(q:q, r:4)
-        XCTAssertEqualWithAccuracy (tile.center.x, 88.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 228.630706599092, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 88.0, 228.630706599092);
 
     }
 
@@ -134,28 +116,22 @@ class TestHexCenterPointsEvenFlat6x6: XCTestCase {
         var tile : Tile!
 
         tile = HexKit.sharedInstance.tile(q:q, r:-2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 121.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 19.0525588832576, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 121.0, 19.0525588832576);
 
         tile = HexKit.sharedInstance.tile(q:q, r:-1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 121.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 57.157676649773, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 121.0, 57.157676649773);
 
         tile = HexKit.sharedInstance.tile(q:q, r:0)
-        XCTAssertEqualWithAccuracy (tile.center.x, 121.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 95.2627944162882, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 121.0, 95.2627944162882);
 
         tile = HexKit.sharedInstance.tile(q:q, r:1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 121.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 133.367912182804, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 121.0, 133.367912182804);
 
         tile = HexKit.sharedInstance.tile(q:q, r:2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 121.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 171.473029949319, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 121.0, 171.473029949319);
 
         tile = HexKit.sharedInstance.tile(q:q, r:3)
-        XCTAssertEqualWithAccuracy (tile.center.x, 121.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 209.578147715834, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 121.0, 209.578147715834);
 
     }
 
@@ -165,28 +141,22 @@ class TestHexCenterPointsEvenFlat6x6: XCTestCase {
         var tile : Tile!
 
         tile = HexKit.sharedInstance.tile(q:q, r:-2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 154.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 38.1051177665153, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 154.0, 38.1051177665153);
 
         tile = HexKit.sharedInstance.tile(q:q, r:-1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 154.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 76.2102355330306, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 154.0, 76.2102355330306);
 
         tile = HexKit.sharedInstance.tile(q:q, r:0)
-        XCTAssertEqualWithAccuracy (tile.center.x, 154.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 114.315353299546, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 154.0, 114.315353299546);
 
         tile = HexKit.sharedInstance.tile(q:q, r:1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 154.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 152.420471066061, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 154.0, 152.420471066061);
 
         tile = HexKit.sharedInstance.tile(q:q, r:2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 154.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 190.525588832576, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 154.0, 190.525588832576);
 
         tile = HexKit.sharedInstance.tile(q:q, r:3)
-        XCTAssertEqualWithAccuracy (tile.center.x, 154.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 228.630706599092, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 154.0, 228.630706599092);
 
     }
 
@@ -196,28 +166,29 @@ class TestHexCenterPointsEvenFlat6x6: XCTestCase {
         var tile : Tile!
 
         tile = HexKit.sharedInstance.tile(q:q, r:-3)
-        XCTAssertEqualWithAccuracy (tile.center.x, 187.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 19.0525588832576, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 187.0, 19.0525588832576);
 
         tile = HexKit.sharedInstance.tile(q:q, r:-2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 187.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 57.157676649773, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 187.0, 57.157676649773);
 
         tile = HexKit.sharedInstance.tile(q:q, r:-1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 187.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 95.2627944162882, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 187.0, 95.2627944162882);
 
         tile = HexKit.sharedInstance.tile(q:q, r:0)
-        XCTAssertEqualWithAccuracy (tile.center.x, 187.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 133.367912182804, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 187.0, 133.367912182804);
 
         tile = HexKit.sharedInstance.tile(q:q, r:1)
-        XCTAssertEqualWithAccuracy (tile.center.x, 187.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 171.473029949319, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 187.0, 171.473029949319);
 
         tile = HexKit.sharedInstance.tile(q:q, r:2)
-        XCTAssertEqualWithAccuracy (tile.center.x, 187.0, accuracy: accuracy, "Expected result not equal");
-        XCTAssertEqualWithAccuracy (tile.center.y, 209.578147715834, accuracy: accuracy, "Expected result not equal");
+        assertTileCenterAtXY(tile, 187.0, 209.578147715834);
+
+    }
+
+    func assertTileCenterAtXY(tile, x, y) {
+
+        XCTAssertEqualWithAccuracy (tile.center.x, x, accuracy: accuracy, "Expected result not equal");
+        XCTAssertEqualWithAccuracy (tile.center.y, y, accuracy: accuracy, "Expected result not equal");
 
     }
 }
